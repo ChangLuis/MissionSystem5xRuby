e44d18b5eecd11ed3f504df9455d5de7071398a9
ChangLuis <somebodyforluis@gmail.com>
2020-01-25 22:32:07 +0800

    Rspec-add config of factorybot and set mission data

---
+++ spec/factories/mission.rb
@@ -0,0 +1,21 @@
+FactoryBot.define do
+  factory :mission1, class: "Mission" do
+    user_id { 1 }
+    title { "Date" }
+    contents { "Dating with 5xRuby" }
+    status { "pending" }
+    priority_order { "high_priority" }
+    initial_time_at { "2020-01-23 08:00".to_datetime }
+    finish_time_at { "2020-01-23 22:00".to_datetime }
+  end
+
+  factory :mission2, class: "Mission" do
+    user_id { 2 }
+    title { "Meeting" }
+    contents { "Meeting with 5xRuby" }
+    status { "pending" }
+    priority_order { "high_priority" }
+    initial_time_at { "2020-01-31 17:00".to_datetime }
+    finish_time_at { "2020-01-31 22:00".to_datetime }
+  end
+end
