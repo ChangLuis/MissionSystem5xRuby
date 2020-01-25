786284d757f27811954d50862369426503a5827e
ChangLuis <somebodyforluis@gmail.com>
2020-01-26 04:02:41 +0800

    Rspec-modify contents of i18n in missions_controller

--- spec/features/user/missions_controller_spec.rb
+++ spec/features/user/missions_controller_spec.rb
@@ -5,15 +5,15 @@ feature "User/missions" do
     scenario 'creation should be successful' do
       visit new_user_mission_path
       within('form') do
-        fill_in I18n.t('mission.title'), with: "Date"
-        fill_in I18n.t('mission.contents'), with: "dating with alison around taipei city."
-        select "high_priority", from: I18n.t('mission.priority_order')
-        select "pending", from: I18n.t('mission.status')
-        fill_in I18n.t('mission.initial_time_at'), with: "2020-01-23 08:00".to_datetime
-        fill_in I18n.t('mission.finish_time_at'), with: "2020-01-23 22:00".to_datetime
+        fill_in t('activerecord.attributes.mission.title'), with: "Date"
+        fill_in t('activerecord.attributes.mission.contents'), with: "dating with alison around taipei city."
+        select "high_priority", from: t('activerecord.attributes.mission.priority_order')
+        select "pending", from: t('activerecord.attributes.mission.status')
+        fill_in t('activerecord.attributes.mission.initial_time_at'), with: "2020-01-23 08:00".to_datetime
+        fill_in t('activerecord.attributes.mission.finish_time_at'), with: "2020-01-23 22:00".to_datetime
       end
-      click_button I18n.t('submit')
-      expect(page).to have_content("Create sucessfully!")
+      click_button t('submit')
+      expect(page).to have_content(t('mission.create.notice'))
     end
   end
 
@@ -22,11 +22,11 @@ feature "User/missions" do
       mission = Mission.find_by title: "Date"
       visit edit_user_mission_path(mission)
       within('form') do
-        fill_in I18n.t('mission.initial_time_at'), with: "2020-01-24 08:00".to_datetime
-        fill_in I18n.t('mission.finish_time_at'), with: "2020-01-24 22:00".to_datetime
+        fill_in t('activerecord.attributes.mission.initial_time_at'), with: "2020-01-24 08:00".to_datetime
+        fill_in t('activerecord.attributes.mission.finish_time_at'), with: "2020-01-24 22:00".to_datetime
       end
-      click_button I18n.t('submit')
-      expect(page).to have_content("Update successfully!")
+      click_button t('submit')
+      expect(page).to have_content(t('mission.edit.notice'))
       expect(page).to have_content(Date.parse("2020-01-24 08:00"))
       expect(page).to have_content(Date.parse("2020-01-24 22:00"))
     end
@@ -37,9 +37,9 @@ feature "User/missions" do
       mission = Mission.find_by title: "Date"
       visit user_mission_path(mission)
       page.accept_alert do
-        click_link I18n.t("mission.destroy_file")
+        click_link t("mission.destroy.btn")
       end
-      expect(page).to have_content("Destroy successfully!")
+      expect(page).to have_content(t('mission.destroy.notice'))
     end
   end
 
@@ -48,11 +48,11 @@ feature "User/missions" do
     let!(:mission2) { create(:mission2) }
     before(:each) { visit user_missions_path }
     scenario 'should be correct' do
-      expect(page.body.index(mission1.title)).to be < page.body.index(mission2.title)
+      expect(body.index(mission1.title)).to be < body.index(mission2.title)
     end
 
     scenario 'should be incorrect' do
-      expect(page.body.index(mission1.title)).not_to be > page.body.index(mission2.title)
+      expect(body.index(mission1.title)).not_to be > body.index(mission2.title)
     end
   end
 end
