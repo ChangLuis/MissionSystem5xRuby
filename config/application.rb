41ad0c391963218d7033598447362a3bf931ee50
ChangLuis <somebodyforluis@gmail.com>
2020-01-25 13:57:29 +0800

    Config-set default_locale with zh-TW of i18n

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module MissionSystem5xRuby
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.i18n.defualt_locale = "zh-TW"
+    config.i18n.default_locale = "zh-TW"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
