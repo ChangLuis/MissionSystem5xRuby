0320d4d77ffd7a3a16e10f4b230c8ca92e0e45ca
ChangLuis <somebodyforluis@gmail.com>
2020-01-26 18:26:21 +0800

    Routes-add custom url of order_change

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :user do
     resources :missions
+    get 'missions/sort/:order_change', to: 'missions#index', as: 'order_change'
   end
 
   root to: 'user/missions#index'
