e0ed089478641e47a5124167bec40240aa215e9d
ChangLuis <somebodyforluis@gmail.com>
2020-01-28 21:08:21 +0800

    Database-add migration for adding default value with column of is_admin in User

---
+++ db/migrate/20200128124542_add_default_value_to_user_is_admin.rb
@@ -0,0 +1,5 @@
+class AddDefaultValueToUserIsAdmin < ActiveRecord::Migration[6.0]
+  def change
+    change_column(:users, :is_admin, :boolean, default: false, null: false)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_18_145201) do
+ActiveRecord::Schema.define(version: 2020_01_28_124542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2020_01_18_145201) do
     t.string "account", null: false
     t.string "name", null: false
     t.string "password_digest", null: false
-    t.boolean "is_admin", null: false
+    t.boolean "is_admin", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
