8982bbc43ebe6826184d0bd124eaab6d4920c7f9
ChangLuis <somebodyforluis@gmail.com>
2020-01-28 21:04:51 +0800

    Model-add basic validation and related to missions in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  validates :account, presence: true, uniqueness: { case_sensitive: true }
+  validates :name, presence: true, length: { in: 2..30 }
+  validates :password, presence: true, length: { in: 8..20 }
+
+  has_secure_password
+
+  has_many :missions, primary_key: :id, foreign_key: :user_id
 end
