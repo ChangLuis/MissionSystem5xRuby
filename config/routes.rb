9164f2b95a86985985e0aee156cbc42c8e14287c
ChangLuis <somebodyforluis@gmail.com>
2020-01-29 12:31:04 +0800

    Config-add routes of login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'login#new', as: "login"
+  post '/', to: 'login#create'
+  delete 'logout', to: 'login#destroy'
+
   namespace :user do
     resources :missions
     get 'missions/sort/:order_change', to: 'missions#index', as: 'order_change'
   end
-
-  root to: 'user/missions#index'
 end
