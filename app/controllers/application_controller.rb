ec0941391e883a4fbd9be0f05db518551777e8f0
ChangLuis <somebodyforluis@gmail.com>
2020-01-29 12:44:33 +0800

    Controller-add current_user, logged_in? and require_login method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def require_login
+    redirect_to login_path, notice: "您還未登入" unless logged_in?
+  end
 end
