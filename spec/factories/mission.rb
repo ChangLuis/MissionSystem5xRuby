c224d0fced3c1bff65c599aeb2f5e5694f08798c
ChangLuis <somebodyforluis@gmail.com>
2020-01-30 16:37:44 +0800

    Rspec-add more missions for different user

--- spec/factories/mission.rb
+++ spec/factories/mission.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :mission1, class: "Mission" do
+  factory :mission_date, class: "Mission" do
     user_id { 1 }
     title { "Date" }
     contents { "Dating with 5xRuby" }
@@ -9,10 +9,30 @@ FactoryBot.define do
     finish_time_at { "2020-01-31 22:00".to_datetime }
   end
 
-  factory :mission2, class: "Mission" do
-    user_id { 2 }
+  factory :mission_meeting, class: "Mission" do
+    user_id { 1 }
     title { "Meeting" }
     contents { "Meeting with 5xRuby" }
+    status { "pending" }
+    priority_order { "high_priority" }
+    initial_time_at { "2020-01-25 17:00".to_datetime }
+    finish_time_at { "2020-01-25 22:00".to_datetime }
+  end
+
+  factory :mission_wakeup, class: "Mission" do
+    user_id { 2 }
+    title { "Wake up" }
+    contents { "wake up in special day" }
+    status { "finish" }
+    priority_order { "nomal_priority" }
+    initial_time_at { "2020-01-23 08:00".to_datetime }
+    finish_time_at { "2020-01-31 22:00".to_datetime }
+  end
+
+  factory :mission_sport, class: "Mission" do
+    user_id { 2 }
+    title { "Sport" }
+    contents { "Sport with 5xRuby" }
     status { "pending" }
     priority_order { "high_priority" }
     initial_time_at { "2020-01-25 17:00".to_datetime }
