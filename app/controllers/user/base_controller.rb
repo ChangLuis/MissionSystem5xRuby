11dc9c34a3736f7d150312a787b5ec6abbe0ed60
ChangLuis <somebodyforluis@gmail.com>
2020-01-31 01:31:32 +0800

    Controller-add require_user_role in user::base

--- app/controllers/user/base_controller.rb
+++ app/controllers/user/base_controller.rb
@@ -1,3 +1,10 @@
 class User::BaseController < ApplicationController
   before_action :require_login
+  before_action :require_user_role
+
+  private
+
+  def require_user_role
+    redirect_to login_path, notice: "您不是一般使用者" if current_user.is_admin?
+  end
 end
