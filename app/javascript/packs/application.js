c800ca37d0eb8a34f78adbdedfbd05089caefb7c
ChangLuis <somebodyforluis@gmail.com>
2020-01-31 18:59:20 +0800

    JS-add jquery with fadeout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,16 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 import "bootstrap";
+import "jquery";
 
 require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
 
+$(document).on('turbolinks:load', () => {
+  $('#fade-out-target').fadeOut(3000);
+});
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
