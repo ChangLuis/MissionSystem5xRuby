27149a261b237b08dd8def86374263033d04aab1
ChangLuis <somebodyforluis@gmail.com>
2020-01-31 19:03:40 +0800

    Model-add more validations with password in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,13 @@ class User < ApplicationRecord
 
   validates :account, presence: true, uniqueness: { case_sensitive: true }
   validates :name, presence: true, length: { in: 2..30 }
-  validates :password, presence: true, length: { in: 8..20 }
+
+  PASSWORD_REGEX = /\A[a-zA-Z0-9]+\z/.freeze
+  validates :password,
+            confirmation: true,
+            length: { in: 8..20 },
+            format: { with: PASSWORD_REGEX },
+            allow_nil: true
 
   has_secure_password
 
